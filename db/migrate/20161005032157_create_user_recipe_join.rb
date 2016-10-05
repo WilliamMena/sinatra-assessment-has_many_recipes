9744aeb142fdcbc79dcdb3e58074eb0a55022e0a
WilliamMena <williammena1991@gmail.com>
2016-10-04 23:26:05 -0400

    create joins tables

---
+++ db/migrate/20161005032157_create_user_recipe_join.rb
@@ -0,0 +1,8 @@
+class CreateUserRecipeJoin < ActiveRecord::Migration
+  def change
+    create_table :user_recipes do |t|
+      t.integer :user_id
+      t.integer :recipe_id
+    end
+  end
+end
---
+++ db/migrate/20161005032458_create_recipe_categories_join.rb
@@ -0,0 +1,8 @@
+class CreateRecipeCategoriesJoin < ActiveRecord::Migration
+  def change
+    create_table :recipe_categories do |t|
+      t.integer :recipe_id
+      t.integer :category_id
+    end
+  end
+end
