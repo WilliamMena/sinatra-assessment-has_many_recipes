176c1427511405f796e9378f3443c9e759aade5b
WilliamMena <williammena1991@gmail.com>
2016-10-05 21:45:53 -0400

    add first test

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe ApplicationController do
+
+  describe "Homepage" do
+    it 'loads the homepage' do
+      get '/'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome")
+    end
+  end
+
+
+
+
+end
